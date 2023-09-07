3cb90b4759a3d8186659d7d89e30ae1d45b26442
Chia1104 <yuyuchia7423@gmail.com>
2023-09-08 03:00:12 +0800

    hmmm...

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import { Elysia } from "elysia";
+import { dlopen, FFIType, suffix } from "bun:ffi";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(Bun.env.PORT ?? 3000);
+/**
+ * Hmmm...
+ */
+// import { add } from "./libs/main.rs";
+
+// const version = add(1, 0);
+
+const path = `libmain.${suffix}`
+
+const {
+  symbols: {
+    add, // the function to call
+  },
+} = dlopen(
+  path, // a library name or file path
+  {
+    add: {
+      args: [
+        FFIType.int32_t, // the type of the first argument
+        FFIType.int32_t, // the type of the second argument
+      ],
+      returns: FFIType.int32_t, // the type of the return value
+    },
+  },
+);
+
+const app = new Elysia().get("/", () => `Hello Elysia, Bun (v${add(1, 0)})`).listen(Bun.env.PORT ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
